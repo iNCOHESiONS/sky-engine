bab3b4cf4697e8bb4a80f3a16a27f86d6db5b257
iNCOHESiONS <incohesions@gmail.com>
2026-07-14 22:49:06 -0300

    update Sentinel to better match PEP 661

--- sky/sentinel.py
+++ sky/sentinel.py
@@ -1,12 +1,16 @@
-"""
-Unique sentinel values. Based on PEP 661.
-See: https://peps.python.org/pep-0661/#reference-implementation
-"""
+"""Unique sentinel values based on [PEP 661](https://peps.python.org/pep-0661/#reference-implementation)."""
 
 from __future__ import annotations
 
 import inspect
-from typing import LiteralString, Self, final, override
+from types import UnionType
+from typing import (
+    LiteralString,
+    Self,
+    Union,  # pyright: ignore[reportDeprecated]
+    final,
+    override,
+)
 
 _sentinels: dict[str, Sentinel] = {}
 
@@ -24,31 +28,56 @@ def _get_calling_module_name() -> str:
 
 @final
 class Sentinel:
-    """Class for creating unique sentinel values."""
+    """Unique sentinel values based on [PEP 661](https://peps.python.org/pep-0661/#reference-implementation)."""
+
+    __slots__ = ("_id", "_repr")
 
     _id: str  # pyright: ignore[reportUninitializedInstanceVariable]
+    _repr: str  # pyright: ignore[reportUninitializedInstanceVariable]
 
     @override
     def __new__(
-        cls, name: LiteralString, /, *, module_name: str | None = None
+        cls,
+        name: LiteralString,
+        /,
+        *,
+        module_name: str | None = None,
+        repr: str | None = None,
     ) -> Sentinel:
-        id = f"{module_name or _get_calling_module_name()}-{name}"
+        module = module_name or _get_calling_module_name()
+        id = f"{module}-{name}"
 
         if cached := _sentinels.get(id, None):
             return cached
 
         sentinel = super().__new__(cls)
         sentinel._id = id
+        sentinel._repr = repr or f'{cls.__name__}("{name}", module_name="{module}")'
 
         return _sentinels.setdefault(id, sentinel)
 
+    def __init_subclass__(cls):
+        raise TypeError("Sentinel cannot be used as a subclass")
+
     @override
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}("{self.name}", module_name="{self.module}")'
+        return self._repr
 
     @override
     def __reduce__(self) -> tuple[type[Self], tuple[str, str]]:
         return (self.__class__, (self.name, self.module))
+
+    def __copy__(self) -> Self:
+        return self
+
+    def __deepcopy__(self, _) -> Self:
+        return self
+
+    def __or__(self, other: Self, /) -> UnionType:
+        return Union[self, other]  # pyright: ignore[reportDeprecated]
+
+    def __ror__(self, other: Self, /) -> UnionType:
+        return Union[other, self]  # pyright: ignore[reportDeprecated]
 
     @property
     def name(self) -> str:
